a30d60780622b30a6f366a569a52d7f0aea8de63
andre <andrepatriarca4@gmail.com>
2026-01-17 22:24:35 -0300

    Propriedades (props) e children de componentes no React

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 //PascalCase
 //App
 //ExemploDeComponente
-import './styles/global.css'
-import './styles/theme.css'
-export function App (){
-    console.log('oi')
-    return (
-        <>
-            <h1>oi</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae fugit sed ipsam, sunt nam illo, voluptate eum a possimus at maiores maxime est? Voluptates, doloribus aperiam culpa ipsam doloremque fugiat?</p>
-        </>
-    )
-}
- 
+import './styles/global.css';
+import './styles/theme.css';
+import './components/Heading';
+import { Heading } from './components/Heading';
 
+export function App() {
+  console.log('oi');
+  return (
+    <>
+      <Heading></Heading>
+     
+      
+      <h1>oi</h1>
+
+      <p style={{color:"black",margin:"10px"}}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae fugit
+        sed ipsam, sunt nam illo, voluptate eum a possimus at maiores maxime
+        est? Voluptates, doloribus aperiam culpa ipsam doloremque fugiat?
+      </p>
+    </>
+  );
+}
---
+++ src/components/Heading.tsx
@@ -0,0 +1,6 @@
+import styles from './Heading.module.css';
+
+export function Heading() {
+  console.log();
+  return <h1 className={styles.heading}>heading</h1>;
+}
